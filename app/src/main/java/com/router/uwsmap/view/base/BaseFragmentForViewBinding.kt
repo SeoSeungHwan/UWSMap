3ef6ca548642b8f972d9e8e235cdeec28fbbc735
tmdghks3264 <tmdghks3264@naver.com>
2022-03-01 23:00:49 +0900

    faet. paging, databinding gradle 적용

---
+++ app/src/main/java/com/router/uwsmap/view/base/BaseFragmentForViewBinding.kt
@@ -0,0 +1,35 @@
+package com.router.uwsmap.view.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+
+abstract class BaseFragmentForViewBinding<T: ViewDataBinding>: Fragment() {
+    lateinit var viewDataBinding: T
+
+    abstract val layoutId: Int
+
+    abstract fun init()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewDataBinding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        viewDataBinding.lifecycleOwner = viewLifecycleOwner
+
+        init()
+        return viewDataBinding.root
+    }
+}
